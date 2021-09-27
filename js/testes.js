7322dbf1c9a9ba998821f52c4943322e58aa9a84
Israel Santna <israel100santana@gmail.com>
2021-09-27 15:36:02 -0300

    Sistema de cadastro+login)

---
+++ js/testes.js
@@ -0,0 +1,90 @@
+//var nome=window.prompt("digite seu nome")
+//window.alert("Olá," + nome + "!")  //concatenação (ex: 1 + 1 = 11)
+
+
+//var n1 = Number (window.prompt("digite o primeiro numero"))
+//var n2 = Number (window.prompt("digite o segundo numero"))
+//var res = n1 + n2
+//window.alert("o resulado da soma é:" + res)   ///calcular soma
+
+//if (res == 3) {
+//    window.alert("voce colocou o numero 3 dnv")
+//}
+
+//  var login = "Unip"
+//  var login_dig = (window.prompt("Digite o Login:"))
+
+//  if (login_dig === "Unip") {
+//     var senha = 123
+//     var senha_dig = (window.prompt("Digite a Senha:"))
+//     senha_dig == 123 ?document.write("<h1>Acertou a senha</h1>"): document.write("<h1>Senha inválida</h1>")
+// }
+
+// else{
+//     document.write("<h1>Login inválido</h1>")
+// }
+
+// querySelector é a mesma coisa que >>> getElementById
+// var zero = window.document.querySelector("p#zero1")
+
+// zero.style.color = "blue"
+
+
+/// verdadeiro ou falso ? executar se for verdadeiro : e executar se for falso
+
+
+// if (senha_dig != senha) {
+//     document.write("<h1>Senha inválida</h1>")
+
+// }
+
+// else{
+//     document.write("<h1>Acertou a senha</h1>")
+
+// }
+
+
+// function mudar() {
+
+//     var selec = window.document.getElementById("zero")
+//     selec.innerText = "subs.value"
+
+
+// }
+
+
+//*---TELA DE LOGIN---*
+
+// var login = "Unip"
+// var login_dig
+// var senha = 123
+// var senha_dig
+
+
+// while (login_dig != login) {
+//     login_dig = (window.prompt("Digite o Login:"))
+//     if (login_dig != login) {
+//         alert("Login Inválido.")
+//         login_dig = (window.prompt("Digite o Login:"))
+//     }
+
+
+// }
+
+// while (senha_dig != senha) {
+
+//     senha_dig = (window.prompt("Digite o senha:"))
+
+//     if (senha_dig != senha) {
+
+//         alert("Senha Inválida.")
+//         senha_dig = (window.prompt("Digite a Senha novamente."))
+
+//     }
+
+
+// }
+
+
+
+//*---TELA DE LOGIN---*
